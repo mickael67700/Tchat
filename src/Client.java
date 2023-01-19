4e92ddc259f91b4817b173be278f2007148c44cb
mike <mickael.merlet67@gmail.com>
2023-01-19 13:58:59 +0100

    L'utilisateur doit entrer l'adresse ip du serveur

--- src/Client.java
+++ src/Client.java
@@ -8,23 +8,33 @@ public class Client {
 
     public void start() throws IOException {
         // Code pour démarrer le client.
-        Scanner sc = new Scanner(System.in);
+        Scanner c = new Scanner(System.in);
+        System.out.print("Entrez l'adresse IP du serveur : ");
+        String serverIP = c.nextLine();
+        try {
+            // Utilisez l'adresse IP saisie pour se connecter au serveur
+            Socket s = new Socket(serverIP, 5217);
+            // Code pour envoyer et recevoir des messages
+            Scanner sc = new Scanner(System.in);
+            //
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            //
+            System.out.print("Entrez votre nom d'utilisateur : ");
+            String loginName = sc.nextLine();
+            dos.writeUTF(loginName);
+            //
+            System.out.println(dis.readUTF());
+            System.out.println(dis.readUTF());
+            while (true) {
+                System.out.print("Entrez votre message : ");
+                String msg = sc.nextLine();
+                dos.writeUTF(msg);
+            }
 
-        Socket s = new Socket("127.0.0.1", 5217);
-
-        DataInputStream dis = new DataInputStream(s.getInputStream());
-        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-
-        System.out.print("Entrez votre nom d'utilisateur : ");
-        String loginName = sc.nextLine();
-        dos.writeUTF(loginName);
-
-        System.out.println (dis.readUTF());
-        System.out.println(dis.readUTF());
-        while (true) {
-            System.out.print("Entrez votre message : ");
-            String msg = sc.nextLine();
-            dos.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
     }
 }
